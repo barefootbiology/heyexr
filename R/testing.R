67c441fdfb8d2b91b5a41521f5b9c43c8a36fd26
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2015-10-16 09:39:32 -0500

    Commented testing script.

--- R/testing.R
+++ R/testing.R
@@ -7,8 +7,10 @@ library(bit64)
 
 source("~/stonelab/packages/heyexr/R/read_heyex.R")
 
+# Read in a test vol file
 test <- read_heyex("~/stonelab/experiments/oct/oct_atlas/data/laptop-only/2015-09-28/deidentification_test/TEST001.vol")
 
+# Convert the SLO data to a data.frame compatible with ggplot2
 test_slo <- test$slo_image %>%
     as.data.frame %>%
     cbind_rownames("x") %>%
@@ -17,6 +19,7 @@ test_slo <- test$slo_image %>%
     mutate(y = str_replace(y, pattern="V", replacement = "")) %>%
     mutate(y = as.numeric(y), x = as.numeric(as.character(x)))
 
+# Plot the SLO image
 test_slo %>%
     ggplot(aes(x=x, y=y)) + geom_tile(aes(fill = z)) +
     theme_bw() +
@@ -27,4 +30,5 @@ test_slo %>%
           axis.text.y = element_blank()) +
     coord_fixed()
 
+# Show all header information except the "spare" slot.
 test$header[1:26]
