48e4e10f339245d2dc6a63a03aa0c0347a5b106f
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2016-02-16 09:34:22 -0600

    Renamed 'batch_render_slo.R' to 'batch_render_bscans.R'.

--- inst/R/batch_render_bscans.R
+++ inst/R/batch_render_bscans.R
@@ -3,19 +3,22 @@
 # Parallelization based on:
 # http://gforge.se/2015/02/how-to-go-parallel-in-r-basics-tips/
 
-library(parallel)
+# library(parallel)
 library(heyexr)
 library(argparser)
 
 p <- arg_parser("Render OCT summaries in parallel")
 
+p <- add_argument(p, "--vol",
+                  help = "Heidelberg VOL file to parse")
+
 p <- add_argument(p, "--np",
                   help="Number of cores to use",
                   default = 3)
 
-p <- add_argument(p, "--indir",
-                  help="Directory containing VOL files",
-                  default = ".")
+# p <- add_argument(p, "--indir",
+#                   help="Directory containing VOL files",
+#                   default = ".")
 
 p <- add_argument(p, "--outdir",
                   help="Directory to write results",
@@ -23,20 +26,21 @@ p <- add_argument(p, "--outdir",
 
 argv <- parse_args(p)
 
+# Echo the arguments
+cat("Parameters:\n")
+argv %>% (function(x) data.frame(argument = names(argv), value = unlist(argv)))
 
-no_cores <- detectCores() - 1
-cl <- makeCluster(no_cores, type = "FORK")
-clusterEvalQ(cl, library(heyexr))
 
 # For all the VOL files within a directory, generate SLO images.
 # (Capture the nulls from parLapply into a list to prevent them from echoing at the end.)
-temp_list <- parLapply(cl,
-          list.files(argv$indir, full.names = TRUE, pattern="VOL"),
-          render_oct_summary, out_dir = argv$outdir)
-
+# temp_list <- parLapply(cl,
+#           list.files(argv$indir, full.names = TRUE, pattern="VOL"),
 # temp_list <- parLapply(cl,
 #           list.files("~/Desktop/oct_controls", full.names = TRUE, pattern="VOL"),
 #           render_slo, out_dir = "~/Desktop/oct_controls_slo", draw_margins = FALSE)
 
+render_oct_summary(vol_file = argv$vol,
+                   out_dir = argv$outdir,
+                   n_cores = argv$np)
 
-stopCluster(cl)
+cat("Done\n")
