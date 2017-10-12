5641ba5a1038d634659555b4e1d66b365f310067
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2017-10-12 15:42:48 -0500

    Added three extra R functions.

---
+++ R/layout_plot_2.R
@@ -0,0 +1,172 @@
+#' Layout plots
+#'
+#' UPDATE
+#'
+#' @return a list containing the OCT and segmentation data (if requested)
+#'
+#' @export
+#' @importFrom magrittr %>%
+#' @importFrom dplyr filter collect distinct select
+#' @importFrom ggplot2 scale_color_brewer element_text theme ggsave scale_color_manual geom_line geom_segment geom_tile geom_vline aes ggplot annotate ggtitle ggplotGrob
+#' @importFrom gridExtra arrangeGrob
+#' @importFrom grid textGrob arrow
+#' @importFrom parallel parLapply makeCluster clusterCall stopCluster
+#' @importFrom purrr walk
+#' @importFrom ggmap theme_nothing
+layout_plot_2 <- function(b_n, oct, p_slo, layer_y_max, layer_y_min, xml_file,
+                          oct_segmentation, b_n_seg, center_file,
+                          center_z_voxel, center_x_voxel,
+                          overlay_bscan_position,
+                          overlay_heidelberg_segmentation, oct_seg_array,
+                          base_name, output_path, file_type,
+                          tt) {
+
+    # Label the output files in the reverse order as the bscan IDs
+    reverse_order <- oct$header$num_bscans:1
+
+    # Function from http://adv-r.had.co.nz/Exceptions-Debugging.html
+    is.error <- function(x) inherits(x, "try-error")
+
+    # Construct the b-scan plot:
+    # Perform gamma correction to lighten dark values.
+    # Value of gamma recommended by author of Open Heyex plugin for
+    # ImageJ.
+
+
+    p_1 <- construct_bscan(oct, b_n, layer_y_max = layer_y_max,
+                           layer_y_min = layer_y_min,
+                           contrast_correction = spline_correction) +
+        labs(x = "", y = "") +
+        tt +
+        theme_nothing()
+
+    # If an XML file was provided,
+    # overlay Iowa Reference Algorithms segmentation on the top b-scan.
+    if(!is.null(xml_file)) {
+        p_1_l <- p_1 +
+            geom_line(data=oct_segmentation$layers %>%
+                          filter(bscan_id == b_n_seg[as.character(b_n)]),
+                      mapping = aes(x=ascan_id,
+                                    y=value + 1, # TESTING!!!!
+                                    group=as.factor(layer_y_order),
+                                    color=as.factor(layer_y_order)),
+                      alpha=0.6) +
+            scale_color_brewer(name="boundary", palette = "Paired")
+    } else {
+        p_1_l <- p_1
+    }
+
+    # Add a vertical line to indicate the position of the
+    # manually annotated center (e.g., fovea).
+    if(!is.null(center_file)) {
+        if(b_n == center_z_voxel) {
+            p_1_l <- p_1_l +
+                geom_vline(xintercept = center_x_voxel,
+                           color = "green",
+                           alpha = 0.5,
+                           linetype = "dotted")
+        }
+    }
+
+    # Overlay Heidelberg segmentation on the lower b-scan plot
+    if(overlay_heidelberg_segmentation & !is.error(oct_seg_array)) {
+        n_segments <- oct_seg_array %>%
+            dplyr::filter(b_scan == b_n) %>%
+            select(seg_layer) %>%
+            distinct() %>%
+            collect %>%
+            .[["seg_layer"]]
+
+        segmentation_layer_value <- c("1"="red","2"="blue","3"="green")[as.character(n_segments)]
+
+        p_1_l2 <- p_1 +
+            geom_line(data = oct_seg_array %>% dplyr::filter(b_scan == b_n),
+                      mapping = aes(group=as.factor(seg_layer),
+                                    color=as.factor(seg_layer)),
+                      alpha = 0.5) +
+            scale_color_manual(guide = 'none',
+                               values = segmentation_layer_value)
+    } else {
+        p_1_l2 <- p_1
+    }
+
+    p_layout <- NULL
+    width <- NULL
+
+    # If you are to include the SLO
+    if(p_slo != FALSE) {
+        # And if the b-scans should overlay the IR image
+        if(overlay_bscan_position) {
+            # Overlay the position of the b-scans on the top SLO
+            p_slo_1 <- p_slo +
+                # Draw the other b-scans but not the current one
+                geom_segment(data = oct$bscan_headers %>%
+                                 filter(bscan != b_n),
+                             mapping = aes(x = start_x_pixels,
+                                           xend = end_x_pixels,
+                                           y = start_y_pixels,
+                                           yend = end_y_pixels),
+                             color = "darkgreen",
+                             alpha = 0.5) +
+
+                # Draw the current b-scan
+                geom_segment(data = oct$bscan_headers %>%
+                                 filter(bscan == b_n),
+                             mapping = aes(x = start_x_pixels,
+                                           xend = end_x_pixels,
+                                           y = start_y_pixels,
+                                           yend = end_y_pixels),
+                             color = "green",
+                             alpha = 0.9,
+                             arrow = arrow(angle = 15, length = unit(0.1, "inches"), type = "closed")) +
+
+                labs(x = "", y = "") +
+                tt +
+                theme_nothing()
+        } else {
+            p_slo_1 <- p_slo +
+                labs(x = "", y = "") +
+                tt +
+                theme_nothing()
+
+        }
+
+        # Add in the grid center information if available
+        if(!is.null(center_file)) {
+            p_slo_1 <- p_slo_1 +
+                annotate("point",
+                         x = center_x,
+                         y = center_y,
+                         color = "green",
+                         alpha = 0.5)
+        }
+
+        # Set the height of the b-scan to that of the SLO
+        g_slo_1 <- ggplotGrob(p_slo_1)
+
+        g_1_l <- ggplotGrob(p_1_l)
+
+        g_1_l$heights <- g_slo_1$heights
+
+        # Layout the plots
+        p_layout <- arrangeGrob(g_slo_1 ,
+                                g_1_l ,
+                                ncol=2,
+                                widths = c(4, 6))
+
+        width <- 10
+
+    } else {
+        p_layout <- p_1_l
+        width <- 10 - 4
+    }
+
+    # Save the plot as a layout
+    file_out <- paste(output_path, "/", base_name, "_",
+                      sprintf("%03d", reverse_order[b_n]), ".",
+                      file_type, sep="")
+
+    ggsave(filename = file_out, plot = p_layout,
+           units = "in", width = width, height = 4, dpi = 300)
+
+}
---
+++ R/read_heyex_raw.R
@@ -0,0 +1,145 @@
+#' Read a Heidelberg Spectralis VOL file
+#'
+#' Creates a list containing data from a Heidelberg Spectralis VOL file.
+#'
+#' @param vol_file path to VOL file
+#' @param header_slo_only Import only the header information and SLO image?
+#'
+#' @return a list containing the data from the VOL file
+#'
+#' @export
+#' @importFrom magrittr %>%
+#' @importFrom dplyr bind_rows mutate
+#' @importFrom readr read_file_raw
+read_heyex_raw <- function(vol_file, header_slo_only = FALSE) {
+    # Code based on these two projects:
+    #
+    # https://github.com/halirutan/HeyexImport
+    # http://rsb.info.nih.gov/ij/plugins/heyex/index.html
+
+    # # Create a connection to the VOL file
+    # vol_con = file(vol_file, "rb")
+
+    # Read the entire file as a RAW vector, then
+    # create a new connection in memory.
+    vol_con <- read_file_raw(vol_file) %>%
+        rawConnection("rb")
+
+    # Read the header
+    header <- read_heyex_header(vol_con)
+
+    # cat("Offset to slo:",seek(vol_con, where = NA), "\n")
+    # Read the SLO image
+    slo_image <- read_heyex_slo(vol_con, header)
+
+    if(!header_slo_only) {
+        # Calculated offests
+        file_header_size = 2048; #integer, size in bytes of file header, equal to
+                                        # offset to reach SLO image;
+        slo_image_size = header$size_x_slo * header$size_y_slo; # integer, size in
+                                        # bytes of the SLO image data;
+        oct_image_size = header$size_x * header$size_z * 4; # integer, size in
+                                        # bytes of each B-Scan OCT image;
+        first_bscan_header = file_header_size + slo_image_size;
+        bscan_block_size = header$bscan_hdr_size + oct_image_size;
+        # integer, calculates size in bytes of the b-scan block which includes the
+        # b-scan header and the b-scan;
+
+
+        # Create empty containers
+        bscan_header_all <- list()
+        seg_array <- list()
+        bscan_image <- list()
+
+        # TASK: Move this to an external function.
+        # Read in the segmentation arrays
+
+        pb <- txtProgressBar(min = 0,
+                             max = header$num_bscans,
+                             style = 1,
+                             file = stderr())
+
+        for (bscan in c(0:(header$num_bscans-1))) {
+            setTxtProgressBar(pb, bscan + 1, title = "Reading B-Scans")
+            # cat("Reading b-scan header\n")
+            bscan_header_all[[bscan + 1]] <- read_bscan_header(vol_con, header)
+
+            # cat("Entering seg_layer * a_scan nested loop:\n")
+            # Read in the Heidelberg segmentation information
+            for (seg_layer in c(0:(bscan_header_all[[bscan + 1]]$num_seg - 1))) {
+                for (a_scan in c(0:(header$size_x - 1))) {
+                    # R vectors and lists are indexed at 1
+                    index <- 1 +
+                        a_scan +
+                        seg_layer * header$size_x +
+                        bscan * bscan_header_all[[bscan + 1]]$num_seg * header$size_x
+
+                    y_value <- read_float(vol_con)
+                    if ((y_value < 3.4028235E37) & !is.na(y_value)) {
+                        seg_array[index] <- y_value
+                    } else {
+                        seg_array[index] <- NA
+                    }
+                }
+            }
+
+            # NOTE: I think this is simply to move the file buffer along. Not
+            #       sure if I should substitute "readBin" with "seek".
+            temp <- readBin(vol_con, "raw",
+                            n = (header$bscan_hdr_size - 256 - (bscan_header_all[[bscan+1]]$num_seg*header$size_x*4)))
+
+            # bscan_image[[length(bscan_image) + 1]] <- read_float_vector(vol_con, n = header$size_x * header$size_z)
+            bscan_image[[bscan + 1]] <- readBin(vol_con,
+                                                "numeric",
+                                                n = header$size_x * header$size_z,
+                                                size = 4)
+
+            # cat("bscan: ", bscan, "\n")
+            # cat("n = ", header$size_x * header$size_z, "\n")
+            # cat("length of vector = ", length(bscan_image[[bscan + 1]]), "\n")
+
+        }
+
+        # TASK: Convert bscan_headers to a data.frame.
+        #       We can get rid of the "spare" column.
+        bscan_header_all <- lapply(bscan_header_all, function(x) x[1:9] %>%
+                                         unlist %>%
+                                         matrix(nrow = 1, byrow = FALSE) %>%
+                                         data.frame(stringsAsFactors = FALSE)) %>%
+            bind_rows %>%
+            setNames(c("version", "bscan_header_size",
+                       "start_x", "start_y",
+                       "end_x", "end_y", "num_seg",
+                       "off_seg", "quality")) %>%
+            mutate(bscan_header_size = as.numeric(bscan_header_size),
+                   start_x = as.numeric(start_x),
+                   start_y = as.numeric(start_y),
+                   end_x = as.numeric(end_x),
+                   end_y = as.numeric(end_y),
+                   num_seg = as.numeric(num_seg),
+                   off_seg = as.numeric(off_seg),
+                   quality = as.numeric(quality)) %>%
+            mutate(start_x_pixels = start_x / header$scale_x_slo,
+                   start_y_pixels = start_y / header$scale_y_slo,
+                   end_x_pixels = end_x / header$scale_x_slo,
+                   end_y_pixels = end_y / header$scale_y_slo) %>%
+            mutate(bscan = 1:n())
+
+        output <- list(header = header,
+                       slo_image = slo_image,
+                       bscan_headers = bscan_header_all,
+                       seg_array = seg_array,
+                       bscan_images = bscan_image)
+
+    } else {
+        # Return only the header and SLO image
+        output <- list(header = header,
+                       slo_image = slo_image)
+    }
+
+    # Close the connection to the VOL file
+    close(vol_con)
+
+    # Return the requested object
+    return(output)
+}
