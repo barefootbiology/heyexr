c7d2dbfac5e35d1ea34bb3cceafb8165dabd7bd3
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2018-01-29 10:34:22 -0600

    Changed the read_segmentation_xml function to use functions from 'xml2' package instead of from the 'XML' package.

--- R/read_segmentation_xml.R
+++ R/read_segmentation_xml.R
@@ -8,124 +8,88 @@
 #'
 #' @export
 #' @importFrom dplyr tbl_df filter rename mutate mutate group_by ungroup select distinct inner_join bind_rows
-#' @importFrom XML xmlParse xmlRoot xmlValue xmlTreeParse xmlToDataFrame
 #' @importFrom magrittr %>%
+#' @importFrom xml2 read_xml xml_find_first xml_find_all xml_text xml_integer xml_double
 read_segmentation_xml <- function(xml_file) {
+
     oct_file_parsed_tab <- gsub(xml_file, pattern="\\.xml$",
-                               replacement=".txt", perl = TRUE)
+                                replacement=".txt", perl = TRUE)
 
     # Loading the XML version
-    oct_surfaces_xml <- xmlParse(xml_file)
+    # oct_surfaces_xml <- xmlParse(xml_file)
+    oct_surfaces_xml <- read_xml(xml_file)
 
-    # TASK: Update the code to parse through the file sequentially as that may
-    #       save time.
+    # Pull out the general information
+    size_units <- xml_find_first(oct_surfaces_xml,
+                                 ".//scan_characteristics//size//unit") %>%
+        xml_text()
+    size_x <- xml_find_first(oct_surfaces_xml,
+                             ".//scan_characteristics//size//x") %>%
+        xml_integer()
+    size_y <- xml_find_first(oct_surfaces_xml,
+                             ".//scan_characteristics//size//y") %>%
+        xml_integer()
+    size_z <- xml_find_first(oct_surfaces_xml,
+                             ".//scan_characteristics//size//z") %>%
+        xml_integer()
+    voxel_size_units <- xml_find_first(oct_surfaces_xml,
+                                       ".//scan_characteristics//voxel_size//unit") %>%
+        xml_text()
+    voxel_size_x <- xml_find_first(oct_surfaces_xml,
+                                   ".//scan_characteristics//voxel_size//x") %>%
+        xml_double()
+    voxel_size_y <- xml_find_first(oct_surfaces_xml,
+                                   ".//scan_characteristics//voxel_size//y") %>%
+        xml_double()
+    voxel_size_z <- xml_find_first(oct_surfaces_xml,
+                                   ".//scan_characteristics//voxel_size//z") %>%
+        xml_double()
 
-    # Parse the information about scan unit, scan dimensions,
-    # voxel units, and voxel dimensions
-    size_units <- xmlRoot(oct_surfaces_xml)[["scan_characteristics"]][["size"]][["unit"]] %>%
-        xmlValue
-    size_x <- xmlRoot(oct_surfaces_xml)[["scan_characteristics"]][["size"]][["x"]] %>%
-        xmlValue %>%
-        as.numeric
-    size_y <- xmlRoot(oct_surfaces_xml)[["scan_characteristics"]][["size"]][["y"]] %>%
-        xmlValue %>%
-        as.numeric
-    size_z <- xmlRoot(oct_surfaces_xml)[["scan_characteristics"]][["size"]][["z"]] %>%
-        xmlValue %>%
-        as.numeric
+    surface_num <- xml_find_first(oct_surfaces_xml,
+                                  ".//surface_num") %>%
+        xml_integer()
 
-    # This data will be used later for converting voxels to µm for reports.
-    voxel_size_units <- xmlRoot(oct_surfaces_xml)[["scan_characteristics"]][["voxel_size"]][["unit"]] %>%
-        xmlValue
-    voxel_size_x <- xmlRoot(oct_surfaces_xml)[["scan_characteristics"]][["voxel_size"]][["x"]] %>%
-        xmlValue %>%
-        as.numeric
-    voxel_size_y <- xmlRoot(oct_surfaces_xml)[["scan_characteristics"]][["voxel_size"]][["y"]] %>%
-        xmlValue %>%
-        as.numeric
-    voxel_size_z <- xmlRoot(oct_surfaces_xml)[["scan_characteristics"]][["voxel_size"]][["z"]] %>%
-        xmlValue %>%
-        as.numeric
 
-    surface_num <- xmlRoot(oct_surfaces_xml)[["surface_num"]] %>%
-        xmlValue %>%
-        as.numeric
+    # Pull out the surface specific information
+    surface_labels <- xml_find_all(oct_surfaces_xml,
+                                   ".//surface//label") %>%
+        xml_text()
+    surface_names <- xml_find_all(oct_surfaces_xml,
+                                  ".//surface//name") %>%
+        xml_text()
+    surface_instances <- xml_find_all(oct_surfaces_xml,
+                                      ".//surface//instance") %>%
+        xml_text()
 
-    # Parse the XML file if that has not been performed previously.
-    # Otherwise, load a tab-delimited version of the parsed XML.
+    # Pull out the surface y values as a single vector
+    all_surfaces_y <- xml_find_all(oct_surfaces_xml,
+                                   ".//surface//bscan//y") %>%
+        xml_integer()
+
+
     oct_data_frame <- NULL
+
     if(!file.exists(oct_file_parsed_tab)) {
 
-        # CONVERT THE FOLLOWING CODE TO SOME KIND OF APPLY statement or better!
-        # Elements 8-18 in the XML file correspond to the 11 surfaces detected
-        # by Iowa Reference Algorithms.
+        oct_data_frame <- tibble(label = rep(surface_labels, each = size_z * size_x),
+                                 name = rep(surface_names, each = size_z * size_x),
+                                 layer_y_order = factor(name, levels = surface_names) %>%
+                                     as.numeric(),
+                                 bscan_id = rep(rep(1:size_z, each = size_x), times = surface_num),
+                                 ascan_id = rep(rep(1:size_x, times = size_z), times = surface_num),
+                                 value = all_surfaces_y)
 
-        oct_list <- list()
-        # TASK: Update this code to work with any number of surface results
-        surface_index <- xmlTreeParse(xml_file)$doc$children$surfaces %>%
-            names %>%
-            grepl(pattern = "^surface$", perl = TRUE) %>%
-            which
-        for(i in surface_index) {
-            surface_label <- xmlRoot(oct_surfaces_xml)[[i]][[1]] %>%
-                xmlValue() %>%
-                as.numeric
-            surface_name <- xmlRoot(oct_surfaces_xml)[[i]][[2]] %>%
-                xmlValue()
-            # NOTE: The "instance" value is something new with the 4.0.0
-            #       version of the Iowa Reference Algorithms. I'm not sure
-            #       to what it refers.
-            instance_value <- xmlRoot(oct_surfaces_xml)[[i]][[3]] %>%
-                xmlValue()
-
-            # There are 61 "bscan" subelements.
-            # Combine these as a long format table, using 1-61 as the covariate (bscan_id)
-            # CODE HERE
-
-            surface_temp_list <- list()
-            for (j in (1:size_z)+3) {
-                surface_temp_list[[j]] <- xmlRoot(oct_surfaces_xml)[[i]][[j]] %>%
-                    xmlToDataFrame %>%
-                    tbl_df %>%
-                    mutate(label=surface_label, name=surface_name, bscan_id=j-3)
-            }
-            oct_list[[i]] <- bind_rows(surface_temp_list)
-        }
-
-        # Combine all the surfaces into one file
-        oct_data_frame <- bind_rows(oct_list) %>%
-            rename(value=text) %>%
-            mutate(value=as.numeric(value),
-                   bscan_id=as.numeric(bscan_id),
-                   label=as.numeric(label)) %>%
-            mutate(label_2=as.numeric(factor(as.character(label)))) %>%
-            group_by(label) %>%
-            mutate(ascan_id = rep(1:size_x, size_z)) %>%
-            ungroup
-
-        # NOTE: Since 4.0.0, y is now z. So I think this should
-        #       probably be come layer_y_order, but I need to check!
-        # Get the depth rank for each layer
-        layer_y_order <- oct_data_frame %>%
-            select(name, label) %>%
-            distinct %>%
-            mutate(layer_y_order = rank(as.numeric(label))) %>%
-            select(-label)
-
-        # Add that value to the data
-        oct_data_frame <- oct_data_frame %>%
-            inner_join(layer_y_order)
-
-        # EXPORT
         # Save to file for quicker loading
         oct_data_frame %>%
             write.table(oct_file_parsed_tab,
                         sep="\t", quote=FALSE, col.names=TRUE, row.names=FALSE)
 
     } else {
-        oct_data_frame <- read.delim(oct_file_parsed_tab, sep="\t", header=TRUE) %>%
+        oct_data_frame <- read.delim(oct_file_parsed_tab, sep="\t",
+                                     header=TRUE) %>%
             tbl_df
     }
+
 
     # Return a list of all the variables
     list(info = list(file = xml_file,
