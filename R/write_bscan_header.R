73fa9153d8ef191d60466c15636164abf4b905ee
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2019-03-25 15:43:18 -0500

    Add function to write B-scan header.

--- R/write_bscan_header.R
+++ R/write_bscan_header.R
@@ -18,14 +18,14 @@ write_bscan_header <- function(vol_con, bscan_header_list) {
         charToRaw() %>%
         pad_raw(n = 12) %>%
         writeBin(vol_con, "raw", endian = "little")
-    writeBin(bscan_header_list$bscan_hdr_size, vol_con, integer(), endian = "little")
+    writeBin(bscan_header_list$bscan_header_size, vol_con, integer(), endian = "little")
     writeBin(bscan_header_list$start_x, vol_con, double(), endian = "little")
     writeBin(bscan_header_list$start_y, vol_con, double(), endian = "little")
     writeBin(bscan_header_list$end_x, vol_con, double(), endian = "little")
     writeBin(bscan_header_list$end_y, vol_con, double(), endian = "little")
     writeBin(bscan_header_list$num_seg, vol_con, integer(), endian = "little")
     writeBin(bscan_header_list$off_seg, vol_con, integer(), endian = "little")
-    writeBin(bscan_header_list$quality, vol_con, what="numeric", size = 4, endian = "little")
+    writeBin(bscan_header_list$quality, vol_con, size = 4, endian = "little")
     # bscan_header_list$spare %>%
     #     charToRaw() %>%
     #     pad_raw(n = 196) %>%
