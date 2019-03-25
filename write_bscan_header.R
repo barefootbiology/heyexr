57cd8c0ac507926928c1636b3a8933e97a5c4a31
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2019-03-25 14:31:39 -0500

    Specify endianness.

--- write_bscan_header.R
+++ write_bscan_header.R
@@ -17,19 +17,19 @@ write_bscan_header <- function(vol_con, bscan_header_list) {
     bscan_header_list$version %>%
         charToRaw() %>%
         pad_raw(n = 12) %>%
-        writeBin(vol_con, "raw")
-    writeBin(bscan_header_list$bscan_hdr_size, vol_con, integer())
-    writeBin(bscan_header_list$start_x, vol_con, double())
-    writeBin(bscan_header_list$start_y, vol_con, double())
-    writeBin(bscan_header_list$end_x, vol_con, double())
-    writeBin(bscan_header_list$end_y, vol_con, double())
-    writeBin(bscan_header_list$num_seg, vol_con, integer())
-    writeBin(bscan_header_list$off_seg, vol_con, integer())
-    writeBin(bscan_header_list$quality, vol_con, what="numeric", size = 4)
+        writeBin(vol_con, "raw", endian = "little")
+    writeBin(bscan_header_list$bscan_hdr_size, vol_con, integer(), endian = "little")
+    writeBin(bscan_header_list$start_x, vol_con, double(), endian = "little")
+    writeBin(bscan_header_list$start_y, vol_con, double(), endian = "little")
+    writeBin(bscan_header_list$end_x, vol_con, double(), endian = "little")
+    writeBin(bscan_header_list$end_y, vol_con, double(), endian = "little")
+    writeBin(bscan_header_list$num_seg, vol_con, integer(), endian = "little")
+    writeBin(bscan_header_list$off_seg, vol_con, integer(), endian = "little")
+    writeBin(bscan_header_list$quality, vol_con, what="numeric", size = 4, endian = "little")
     # bscan_header_list$spare %>%
     #     charToRaw() %>%
     #     pad_raw(n = 196) %>%
     #     writeBin(vol_con, "raw")
-    writeBin(raw(196), vol_con, "raw")
+    writeBin(raw(196), vol_con, "raw", endian = "little")
 
 }
