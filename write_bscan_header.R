a0c776800ce0c4d317f7149d8879f110dc3b7f11
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2019-03-25 11:56:41 -0500

    Add function to write out b-scan header.

---
+++ write_bscan_header.R
@@ -0,0 +1,35 @@
+#' Write b-scan header information to a file connection
+#'
+#' Writes the b-scan header information to a file connection.
+#'
+#' @param vol_con the connection to a VOL file
+#' @param bscan_header_list previously imported b-scan header data
+#'
+#' @export
+#' @importFrom magrittr %>%
+write_bscan_header <- function(vol_con, bscan_header_list) {
+    # For each B-Scan, read header and data
+    # bscan_header_list <- list()
+
+    # IMPLEMENT HEADER READING HERE.
+    # CODE DIRECTLY FROM Open_Heyex_Raw.java
+    # Read the first header.
+    bscan_header_list$version %>%
+        charToRaw() %>%
+        pad_raw(n = 12) %>%
+        writeBin(vol_con, "raw")
+    writeBin(bscan_header_list$bscan_hdr_size, vol_con, integer())
+    writeBin(bscan_header_list$start_x, vol_con, double())
+    writeBin(bscan_header_list$start_y, vol_con, double())
+    writeBin(bscan_header_list$end_x, vol_con, double())
+    writeBin(bscan_header_list$end_y, vol_con, double())
+    writeBin(bscan_header_list$num_seg, vol_con, integer())
+    writeBin(bscan_header_list$off_seg, vol_con, integer())
+    writeBin(bscan_header_list$quality, vol_con, what="numeric", size = 4)
+    # bscan_header_list$spare %>%
+    #     charToRaw() %>%
+    #     pad_raw(n = 196) %>%
+    #     writeBin(vol_con, "raw")
+    writeBin(raw(196), vol_con, "raw")
+
+}
