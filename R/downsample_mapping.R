0382925d40166766745a32e6e3c40cc524773d31
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2019-03-28 13:40:51 -0500

    Add functions to downsample volume object.

---
+++ R/downsample_mapping.R
@@ -0,0 +1,54 @@
+#' Downsample a sequence of numbers.
+#'
+#' \code{downsample_mapping} downsamples a sequence of numbers, 1 through
+#' \code{n} and returns a vector of indices, centered on the middle
+#' number of the original sequence.
+#'
+#' @param original_n An integer giving the length of the original sequence of
+#'     numbers.
+#' @param down_n The desired size of the downsampled sequence.
+#'
+#' @return A list containing the downsampling factor and a
+#'     vector a downsampled indices.
+#'
+#' @export
+#' @importFrom magrittr %>%
+downsample_mapping <- function(original_n, down_n) {
+  if(original_n <= down_n) {
+    stop("Downsampling number is greater than or equal to original number!\t")
+  }
+
+  # Center downsampling on the middle b-scan
+  # TASK: Consider what to do in the rare case of a non-odd number.
+  original_center <-  median(1:original_n)
+
+  down_center <- median(1:down_n)
+
+  down_factor <- floor(original_n / down_n)
+
+  half_length <- ceiling(down_n / 2)
+
+  lower_seq <-
+    seq(
+      from = original_center,
+      by = -1 * down_factor,
+      length.out = half_length
+    )
+
+  upper_seq <-
+    seq(
+      from = original_center,
+      by = down_factor,
+      length.out = half_length
+    )
+
+  down_seq <-
+    c(lower_seq, upper_seq) %>%
+    unique() %>%
+    sort() %>%
+    as.integer()
+
+  # TASK: Better would be to return a vector with the down_factor as
+  #       an attribute.
+  list(factor = down_factor, bscan_id = down_seq)
+}
---
+++ R/downsample_vol.R
@@ -0,0 +1,33 @@
+#' Downsample B-scans in a volume object.
+#'
+#' \code{downsample_vol} reduces the number of B-scans in a volume
+#' object. The reduction maintains the central B-scan, and ensures that
+#' the distance between B-scans is constant.
+#'
+#' @param vol A volume object.
+#' @param new_n The number of B-scans to retain.
+#'
+#' @return A volume object.
+#' @export
+#' @importFrom magrittr %>%
+downsample_vol <- function(vol, new_n) {
+
+  vol_down <- vol
+
+  down_mapping <- downsample_mapping(vol$header$num_bscans, new_n)
+
+  vol_down$header$distance <- vol_down$header$distance * down_mapping$factor
+
+  vol_down$header$num_bscans <- new_n
+
+  vol_down$bscan_headers <-
+    vol_down$bscan_headers %>%
+    filter(bscan_id %in% down_mapping$bscan_id) %>%
+    mutate(bscan_id = 1:n())
+
+  vol_down$seg_array <- vol_down$seg_array[,down_mapping$bscan_id,]
+
+  vol_down$bscan_images <- vol_down$bscan_images[,down_mapping$bscan_id,]
+
+  vol_down
+}
