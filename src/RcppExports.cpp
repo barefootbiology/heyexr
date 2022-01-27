a968b127551fe16c39998e42929238d9a7a2583b
S. Scott Whitmore <steven-whitmore@uiowa.edu>
2022-01-27 15:08:38 -0600

    Change in Rccp code.

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // datetime_to_raw
 RawVector datetime_to_raw(int sec_epoch);
 RcppExport SEXP _heyexr_datetime_to_raw(SEXP sec_epochSEXP) {
